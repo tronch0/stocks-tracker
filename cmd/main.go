b5019710d8a2419116ffbb8fecc92dcb073e39dc
tronchoo <ron.kahat@gmail.com>
2021-05-09 22:14:40 +0300

    main and go mod

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"stocks-tracker/service"
+)
+
+func main(){
+	fmt.Printf("** booting stocks-tracker service **\n")
+
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stdout, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	return service.New()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module stocks_tracker
+
+go 1.15
